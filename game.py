2024058f86e1037759c6d1b61101aeaad4139fab
Vitaly <hohlov.vit.kir@gmail.com>
2025-02-04 23:01:28 +0300

    Добавление меню

---
+++ game.py
@@ -0,0 +1,357 @@
+import os
+import sys
+
+import pygame
+
+pygame.display.init()
+size = WIDTH, HEIGHT = 200, 200
+screen = pygame.display.set_mode(size)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def load_image(name, colorkey=None, scale=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((5, 5))
+            image = pygame.transform.scale(image, (50, 50))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'door': load_image('door.jpg', colorkey=-1, scale=True),
+    'money': load_image('money.jpg', colorkey=-1, scale=True),
+    'ship': load_image('ship.jpg', colorkey=-1, scale=True)
+}
+player_image = load_image('player.png')
+tile_width = tile_height = 50
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        global tile_width
+        global tile_height
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    global player_image
+    global tile_width
+    global tile_height
+
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.pos = (pos_x, pos_y)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def move(self, x, y):
+        self.rect = self.image.get_rect().move(tile_width * x + 15, tile_height * y + 5)
+        self.pos = x, y
+
+
+class Game:
+    def __init__(self, level_number):
+        self.levels = ['lvl_1.txt', 'lvl_2.txt', 'lvl_3.txt', 'lvl_4.txt', 'lvl_5.txt']
+        self.level_number = level_number
+        pygame.init()
+        self.FPS = 50
+        self.clock = pygame.time.Clock()
+        self.size = self.WIDTH, self.HEIGHT = 1300, 700
+        self.screen = pygame.display.set_mode(self.size)
+        self.screen.fill((255, 255, 255))
+        self.running = True
+
+        self.fall_delay = 200
+        self.fall_event = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.fall_event, self.fall_delay)
+        self.run_delay = 400
+        self.run_event = pygame.USEREVENT + 2
+        pygame.time.set_timer(self.run_event, self.run_delay)
+        self.run_shift_delay = 300
+        self.run_shift_event = pygame.USEREVENT + 3
+        pygame.time.set_timer(self.run_shift_event, self.run_shift_delay)
+        self.music1 = pygame.mixer.Sound('music1.mp3')
+        self.music2 = pygame.mixer.Sound('music2.mp3')
+        self.music3 = pygame.mixer.Sound('music3.mp3')
+        self.total = 1
+
+        self.sky = pygame.image.load('sky.jpg')
+
+    def start_screen(self):
+
+        intro_text = [
+            "          Правила игры",
+            "Игроку необходимо проходить уровни, преодалевая препятствия",
+            "",
+            "    Препятствия: ",
+            "",
+            "Шипы - при попадании на шипы персонаж погибает и игроку",
+            "приходится проходить уровень повторно",
+            "Дверь - для перехода на следующий уровень, персонажу ",
+            "необходимо добраться до двери",
+            "",
+            "",
+            "",
+            "",
+            "    Управление:",
+            "",
+            "Движение вперед [D]",
+            "Движение назад [A]",
+            "Прыжок [Enter]",
+            "",
+            "Для запуска уровня нажмите [Enter]"
+        ]
+
+        fon = pygame.transform.scale(load_image('fon3.jpg'), (self.WIDTH, self.HEIGHT))
+        self.screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
+
+    def nextgame_screen(self):
+        a = f'Количество попыток: {self.total}'
+        intro_text = []
+
+        fon = pygame.transform.scale(load_image('fon2.jpg'), (self.WIDTH, self.HEIGHT))
+        self.screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 50)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
+
+    def winner_screen(self):
+
+        intro_text = ['']
+
+        fon = pygame.transform.scale(load_image('fon.jpg'), (self.WIDTH, self.HEIGHT))
+        self.screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
+
+    def move_map(self, player, movement):
+        x, y = player.pos
+        if movement == 'up':
+            if (self.level[y - 3][x] == '.' or self.level[y - 3][x] == '@' or self.level[y - 3][x] == '&') and \
+                    self.level[y + 1][
+                        x] == '#' and (
+                    self.level[y - 2][x] == '.' or self.level[y - 2][x] == '@' or self.level[y - 2][x] == '&') and (
+                    self.level[y - 1][x] == '.' or self.level[y - 1][x] == '@' or self.level[y - 1][x] == '&') and (
+                    self.level[y - 4][x] == '.' or self.level[y - 4][x] == '@' or self.level[y - 4][x] == '&'):
+                player.move(x, y - 4)
+            elif (self.level[y - 3][x] == '.' or self.level[y - 3][x] == '@' or self.level[y - 3][x] == '&') and \
+                    self.level[y + 1][
+                        x] == '#' and (
+                    self.level[y - 2][x] == '.' or self.level[y - 2][x] == '@' or self.level[y - 2][x] == '&') and (
+                    self.level[y - 1][x] == '.' or self.level[y - 1][x] == '@' or self.level[y - 1][x] == '&'):
+                player.move(x, y - 3)
+            elif self.level[y + 1][x] == '#' and (
+                    self.level[y - 2][x] == '.' or self.level[y - 2][x] == '@' or self.level[y - 2][x] == '&') and (
+                    self.level[y - 1][x] == '.' or self.level[y - 1][x] == '@' or self.level[y - 1][x] == '&') and \
+                    self.level[y + 1][
+                        x] == '#':
+                player.move(x, y - 2)
+            elif (self.level[y - 1][x] == '.' or self.level[y - 1][x] == '@' or self.level[y - 1][x] == '&') and \
+                    self.level[y + 1][
+                        x] == '#':
+                player.move(x, y - 1)
+        if movement == 'down':
+            if self.level[y + 1][x] == '.' or self.level[y + 1][x] == '@' or self.level[y + 1][x] == '&' or \
+                    self.level[y + 1][x] == '!':
+                if self.level[y + 1][x] == '!':
+                    self.music1.stop()
+                    self.music2.play()
+                    self.total += 1
+                    self.nextgame_screen()
+
+                    self.run()
+                else:
+                    player.move(x, y + 1)
+        if movement == 'left':
+            if self.level[y][x - 1] == '.' or self.level[y][x - 1] == '@' or self.level[y][x - 1] == '&' or \
+                    self.level[y][x - 1] == '!' or self.level[y][x - 1] == '*':
+                if self.level[y][x - 1] == '!':
+                    self.music1.stop()
+                    self.music2.play()
+                    self.total += 1
+                    self.nextgame_screen()
+
+                    self.run()
+                else:
+                    player.move(x - 1, y)
+        if movement == 'right':
+            if self.level[y][x + 1] == '.' or self.level[y][x + 1] == '@' or self.level[y][x + 1] == '&' or \
+                    self.level[y][x + 1] == '!' or self.level[y][x - 1] == '*':
+                if self.level[y][x + 1] == '!':
+                    self.music1.stop()
+                    self.music2.play()
+                    self.total += 1
+                    self.nextgame_screen()
+
+                    self.run()
+
+                else:
+                    player.move(x + 1, y)
+        if self.level[y][x] == '&':
+            self.level_number += 1
+            self.run()
+        if self.level[y][x + 1] == '*':
+            self.music1.stop()
+            self.music3.play()
+            self.winner_screen()
+
+    def generate_level(self, level):
+        new_player, x, y = None, None, None
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                if y != 0:
+                    if level[y][x] == '#':
+                        Tile('wall', x, y)
+                        # добавим спрайт в группу
+                    elif level[y][x] == '@':
+                        new_player = Player(x, y)
+                    elif level[y][x] == '&':
+                        Tile('door', x, y)
+                    elif level[y][x] == '!':
+                        Tile('ship', x, y)
+                    elif level[y][x] == '*':
+                        Tile('money', x, y)
+        # вернем игрока, а также размер поля в клетках
+        return new_player, x, y
+
+    def run(self):
+        self.music2.stop()
+        self.music1.stop()
+        self.music1.play(-1)
+        all_sprites.empty()
+        tiles_group.empty()
+        player_group.empty()
+        self.screen.blit(self.sky, (0, 0))
+
+        self.player = None
+        level = self.levels[self.level_number]
+        self.level = load_level(level)
+        self.player, self.level_x, self.level_y = self.generate_level(self.level)
+
+        while self.running:
+            for event in pygame.event.get():
+                keys = pygame.key.get_pressed()
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == self.fall_event:
+                    self.move_map(self.player, 'down')
+                if keys[pygame.K_LSHIFT] == 1 and keys[pygame.K_d] == 1 and event.type == self.run_shift_event:
+                    self.move_map(self.player, 'right')
+                if keys[pygame.K_LSHIFT] == 1 and keys[pygame.K_a] == 1 and event.type == self.run_shift_event:
+                    self.move_map(self.player, 'left')
+                if keys[pygame.K_d] == 1 and event.type == self.run_event:
+                    self.move_map(self.player, 'right')
+                if keys[pygame.K_a] == 1 and event.type == self.run_event:
+                    self.move_map(self.player, 'left')
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.move_map(self.player, 'up')
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_t:
+                        continue
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_d] == 1:
+                self.player.image = load_image('right_mario.png')
+            elif keys[pygame.K_a] == 1:
+                self.player.image = load_image('left_mario.png')
+            else:
+                self.player.image = load_image('player.png')
+            x, y = self.player.pos
+            self.screen.blit(self.sky, (0, 0))
+            all_sprites.draw(self.screen)
+            player_group.draw(self.screen)
+            pygame.display.flip()
+            self.clock.tick(50)
+        pygame.quit()
---
+++ menu.py
@@ -0,0 +1,69 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QMainWindow
+from PyQt6.QtGui import QFont, QFontDatabase
+import pygame
+from game import Game
+
+
+
+
+
+class Menu(QMainWindow):
+    def __init__(self):
+        pygame.init()
+        self.levels_music = pygame.mixer.Sound('levels_window.mp3')
+        self.levels_music.play(-1)
+        super().__init__()
+        self.initUI()
+        QFontDatabase.addApplicationFont('supermario286rusbylyajka.otf')
+        self.centralwidget = QWidget()
+        self.setCentralWidget(self.centralwidget)
+        self.button1 = QPushButton('lEVEL 1', self)
+        self.button1.resize(200, 50)
+        self.button1.move(150, 200)
+        self.button1.setFont(QFont('Super Mario 286(RUS BY LYAJKA)', 20))
+        self.button1.setStyleSheet("background-color: rgb(255, 255, 102)")
+        self.button3 = QPushButton('lEVEL 2', self)
+        self.button3.resize(200, 50)
+        self.button3.move(150, 260)
+        self.button3.setFont(QFont('Super Mario 286(RUS BY LYAJKA)', 20))
+        self.button3.setStyleSheet("background-color: rgb(255, 255, 102)")
+        self.button2 = QPushButton('lEVEL 3', self)
+        self.button2.resize(200, 50)
+        self.button2.move(150, 320)
+        self.button2.setStyleSheet("background-color: rgb(255, 255, 102)")
+        self.button2.setFont(QFont('Super Mario 286(RUS BY LYAJKA)', 20))
+        self.button4 = QPushButton('lEVEL 4', self)
+        self.button4.resize(200, 50)
+        self.button4.move(150, 380)
+        self.button4.setFont(QFont('Super Mario 286(RUS BY LYAJKA)', 20))
+        self.button4.setStyleSheet("background-color: rgb(255, 255, 102)")
+        self.button5 = QPushButton('lEVEL 5', self)
+        self.button5.resize(200, 50)
+        self.button5.move(150, 440)
+        self.button5.setFont(QFont('Super Mario 286(RUS BY LYAJKA)', 20))
+        self.button5.setStyleSheet("background-color: rgb(255, 255, 102)")
+        self.title = QLabel('УРОВНИ', self)
+        self.title.resize(600, 90)
+        self.title.move(130, 80)
+        self.title.setFont(QFont('Super Mario 286(RUS BY LYAJKA)', 60))
+        self.title.setStyleSheet("color: rgb(243, 98, 35)")
+        self.button1.clicked.connect(lambda: self.start_game(0))
+        self.button2.clicked.connect(lambda: self.start_game(1))
+        self.button3.clicked.connect(lambda: self.start_game(2))
+        self.button4.clicked.connect(lambda: self.start_game(3))
+        self.button5.clicked.connect(lambda: self.start_game(4))
+    def initUI(self):
+        self.setGeometry(500, 200, 900, 600)
+        self.setWindowTitle('Главное меню')
+
+
+    def start_game(self, number_level):
+        self.hide()
+        self.levels_music.stop()
+        pygame.init()
+        game = Game(number_level)
+        game.start_screen()
+        game.run()
+        pygame.quit()
+        self.show()
